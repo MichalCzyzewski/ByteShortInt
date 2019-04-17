e88eb9dbff83d3823ed32f24499155afea234a4d
Michal Czyzewski <czy.mich@gmail.com>
2019-04-17 11:09:28 +0200

    casting int to byte

--- src/pl/mczyzewski/main/Main.java
+++ src/pl/mczyzewski/main/Main.java
@@ -7,13 +7,15 @@ public class Main {
         //int has a width of 32
         int myMinValue = -2147483648;
         int myMaxValue = 2147483647;
+        int myTotal = (myMaxValue / 2);
         //byte has  a width of 8
         byte myByteValue = -127;
-                        // byte has a width of 15
+        byte myNewByteValue = (byte)(myByteValue/2);
+        // byte has a width of 15
         short mySortValue = 32767;
         //long haw a width of 64
-        long myLongValue = 100L;
+        long myLongValue = 13323456545600L;
+                                  
 
-        
     }
 }
