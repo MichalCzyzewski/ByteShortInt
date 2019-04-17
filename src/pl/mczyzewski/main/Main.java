88e9ae2f1cb29a09730b12acd2fb9a6ef77e8aef
Michal Czyzewski <czy.mich@gmail.com>
2019-04-17 12:43:49 +0200

    result from the udemy course

--- src/pl/mczyzewski/main/Main.java
+++ src/pl/mczyzewski/main/Main.java
@@ -12,7 +12,7 @@ public class Main {
         int myIntegerNumber = 23334443;
         //4.    Create a varible of type long, and make it equal to 50000 + 10 times
         //      to sum of the byte, plus the short plus the int
-        long totalValue = 5000 + (myByteNumber * 10) + myShortNumber + myIntegerNumber;
+        long totalValue = 50000L + 10L * (myByteNumber + myShortNumber + myIntegerNumber;
 
         System.out.println(totalValue);
     }
