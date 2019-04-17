27c87b55d8beed0b5278efd474440d6fa744c68f
Michal Czyzewski <czy.mich@gmail.com>
2019-04-17 11:03:28 +0200

    add description and long type value

--- src/pl/mczyzewski/main/Main.java
+++ src/pl/mczyzewski/main/Main.java
@@ -3,12 +3,16 @@ package pl.mczyzewski.main;
 public class Main {
 
     public static void main(String[] args) {
+
+        //int has a width of 32
         int myMinValue = -2147483648;
         int myMaxValue = 2147483647;
-
+        //byte has  a width of 8
         byte myByteValue = -127;
-
+                        // byte has a width of 15
         short mySortValue = 32767;
+        //long haw a width of 64
+        long myLongValue = 100L;
 
         
     }
