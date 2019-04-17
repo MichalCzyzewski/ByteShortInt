e464db5d675b11007c14704f2de59c76d4e6140d
Michal Czyzewski <czy.mich@gmail.com>
2019-04-17 12:30:54 +0200

    Test

--- src/pl/mczyzewski/main/Main.java
+++ src/pl/mczyzewski/main/Main.java
@@ -4,20 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //int has a width of 32
-        int myMinValue = -2147483648;
-        int myMaxValue = 2147483647;
-        int myTotal = (myMaxValue / 2);
-        System.out.println("myTotal is "+ myTotal);
-        //byte has  a width of 8
-        byte myByteValue = -127;
-        byte myNewByteValue = (byte)(myByteValue/2);
-        System.out.println("myNewByteValue is " + myNewByteValue);
-                // byte has a width of 15
-        short mySortValue = 32767;
-        //long haw a width of 64
-        long myLongValue = 13323456545600L;
-                                  
+        //1.    Create a byte veriable and set it to any valid byte number
+        byte myByteNumber = 120;
+        //2.    Create a short veriable and set it to any valid byte number
+        short myShortNumber = 22222;
+       //3. Create a integer veriable and set it to any valid byte number
+        int myIntegerNumber = 23334443;
+        //4.    Create a varible of type long, and make it equal to 50000 + 10 times
+        //      to sum of the byte, plus the short plus the int
+        long totalValue = (15000 + (myByteNumber * 10) + myShortNumber + myIntegerNumber);
 
+        System.out.println(totalValue);
     }
 }
