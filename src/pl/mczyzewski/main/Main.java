4a8a665668617c2d48dd4ae5233ccfc5e469f6ad
Michal Czyzewski <czy.mich@gmail.com>
2019-04-17 12:36:08 +0200

    fix 15000 to 5000

--- src/pl/mczyzewski/main/Main.java
+++ src/pl/mczyzewski/main/Main.java
@@ -12,8 +12,8 @@ public class Main {
         int myIntegerNumber = 23334443;
         //4.    Create a varible of type long, and make it equal to 50000 + 10 times
         //      to sum of the byte, plus the short plus the int
-        long totalValue = (15000 + (myByteNumber * 10) + myShortNumber + myIntegerNumber);
+        long totalValue = (5000 + (myByteNumber * 10) + myShortNumber + myIntegerNumber);
 
         System.out.println(totalValue);
     }
-}
+}                                 
